8ce527bf5f3d5c04d736c7df4f685daa34782278
hudaokeji <github@hudaokeji.com>
2020-06-08 10:37:53 +0900

    update

--- dist/uncompressed/skylark-devices-points-all.js
+++ dist/uncompressed/skylark-devices-points-all.js
@@ -302,8 +302,45 @@ define('skylark-devices-points/touch',[
     });
   };
 
+
+  function mousy(elm) {
+    var touchToMouse = function(event) {
+        if (event.touches.length > 1) return; //allow default multi-touch gestures to work
+        var touch = event.changedTouches[0];
+        var type = "";
+        
+        switch (event.type) {
+        case "touchstart": 
+            type = "mousedown"; break;
+        case "touchmove":  
+            type="mousemove";   break;
+        case "touchend":   
+            type="mouseup";     break;
+        default: 
+            return;
+        }
+        
+        // https://developer.mozilla.org/en/DOM/event.initMouseEvent for API
+        var simulatedEvent = document.createEvent("MouseEvent");
+        simulatedEvent.initMouseEvent(type, true, true, window, 1, 
+                touch.screenX, touch.screenY, 
+                touch.clientX, touch.clientY, false, 
+                false, false, false, 0, null);
+        
+        touch.target.dispatchEvent(simulatedEvent);
+        event.preventDefault();
+    };
+
+    elm = elm || document;
+
+    elm.addEventListener("touchstart",touchToMouse,true);
+    elm.addEventListener("touchmove",touchToMouse,true);
+    elm.addEventListener("touchend",touchToMouse,true);
+  }
+
   return points.touch = {
-  	isTouchEnabled
+  	isTouchEnabled,
+    mousy
   };
 	
 });
